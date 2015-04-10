ec31dfd3e98639ba0ed7831ddc0a141918ff7dfd
dantezhu <dantezhu@qq.com>
2015-04-10 14:24:15 +0800

    init

---
+++ android/kpush_demo/src/cn/vimer/kpush_demo/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.vimer.kpush_demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

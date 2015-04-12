cb4cebc72a96663face23b8f2b78bf4e17879859
dantezhu <dantezhu@qq.com>
2015-04-13 00:04:18 +0800

    空行

--- backend/kpush/worker/application.py
+++ backend/kpush/worker/application.py
@@ -3,9 +3,11 @@
 from maple import Worker
 from netkit.box import Box
 
+
 def configure_views(app):
     from views import user
     app.register_blueprint(user.bp)
+
 
 def create_app():
     app = Worker(Box)

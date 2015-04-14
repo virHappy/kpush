6a75b3fc8e4dc098d2d00f74a4659b1b71010baa
dantezhu <dantezhu@qq.com>
2015-04-14 18:39:40 +0800

    为了方便测试

--- backend/kpush/web/views/frontend.py
+++ backend/kpush/web/views/frontend.py
@@ -13,10 +13,13 @@ bp = Blueprint('frontend', __name__)
 
 @bp.route('/server/alloc', methods=['POST'])
 def alloc_server():
+    """
+    # 方便测试
     if not request.json_data:
         return jsonify(
             ret=proto.RET_INVALID_PARAMS
         )
+    """
 
     server_table = kit.mongo_client.get_default_database()[current_app.config['MONGO_TB_SERVER']]
 

d9ef0621b7806c62ca1797fef3e63393c8737020
dantezhu <dantezhu@qq.com>
2015-04-18 09:23:14 +0800

    说明

--- android/kpush_demo/src/cn/kpush/LocalConfig.java
+++ android/kpush_demo/src/cn/kpush/LocalConfig.java
@@ -1,10 +1,7 @@
 package cn.kpush;
 
-/**
- * Created by dantezhu on 15-4-18.
- */
 public class LocalConfig {
-    // 自己部署服务器，需要修改这两处地方
+    // 自己部署服务器，需要修改这些变量
     public static final String DOMAIN = "192.168.0.108:5000";
     public static final String SECRET = "tmp_secret";
 }

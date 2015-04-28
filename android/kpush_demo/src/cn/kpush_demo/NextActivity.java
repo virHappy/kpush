b0ea97256effe08dec9fbd46207dc30ed90a33b9
dantezhu <dantezhu@qq.com>
2015-04-28 12:04:36 +0800

    漏了个文件

---
+++ android/kpush_demo/src/cn/kpush_demo/NextActivity.java
@@ -0,0 +1,14 @@
+package cn.kpush_demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dantezhu on 15-4-28.
+ */
+public class NextActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
